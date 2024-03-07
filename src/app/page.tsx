d6184b36e5eaf8fc41ed542e46b7b41a78846568
titan-rocky <rockalbert466@gmail.com>
2024-03-07 13:08:22 +0530

    feat: SEO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: ":bitspace",
+  description: "Bitspace is a dynamic and innovative organization that provides a platform for young and talented coders to showcase their skills by creating and contributing to open-source projects.",
+  openGraph: {
+      title: ":bitspace",
+      description: "Bitspace is a dynamic and innovative organization that provides a platform for young and talented coders to showcase their skills by creating and contributing to open-source projects.",
+      type: "website",
+      url: "https://www.bitspace.org.in",
+      siteName: ":bitspace",
+    },
+    twitter: {
+        card: "summary_large_image",
+        site: "@bitspaceorg",
+    },
+    robots: "all"
+
+};
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
