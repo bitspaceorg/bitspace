0b5db997c43094230a1633663d68a6bfccc013d4
BharathSanjeeviT <bharathsanjeevi.t@gmail.com>
2024-06-08 00:05:21 +0530

    feat: 🐻 events page UI
    Changes Made
    - Added Events page
    - UI updated

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
+import UpcomingEvents from "@/components/Events/Upcomming";
+import { isTeaser } from "@/libs/utils";
+
 export default function Events() {
-	return (
-		<main className="min-h-screen w-full flex items-center justify-center text-7xl font-bold bg-blue">
-		Events
-		</main>
-	);
+  return (
+    <> { isTeaser ?
+      <main className="min-h-screen w-full flex items-center justify-center text-7xl font-bold bg-blue">
+        Events
+      </main> :
+      <main className="max-w-screen min-h-screen flex justify-center items-center graph-paper">
+        <UpcomingEvents />
+      </main>
+    }
+    </>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,7 +24,11 @@ const config: Config = {
       filgen: "fligen",
       mono: "var(--mono-font)",
     },
-    extend: {},
+    extend: {
+      screens: {
+        ps : "375px",
+      }
+    },
   },
   plugins: [],
 };
